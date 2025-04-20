570b1f7e3f98c5a1b5999228f8d7985aff3f8a96
ppongg-gg <138104404+ppongg-gg@users.noreply.github.com>
2025-04-20 22:18:11 +0700

    Update problem_5.sql

--- final_practice_1/problem_5.sql
+++ final_practice_1/problem_5.sql
@@ -3,7 +3,7 @@ CREATE TRIGGER update_customer_point
 AFTER INSERT ON invoice 
 FOR EACH ROW 
 BEGIN 
-	IF (SELECT SUM(salePrice) FROM invoice_item WHERE invoiveID = new.invoiceID) THEN
+	IF (SELECT SUM(salePrice) FROM invoice_item WHERE invoiveID = new.invoiceID) > 1500 THEN
 		UPDATE Customer c
 		JOIN new
 		ON c.cusID = new.cusID
